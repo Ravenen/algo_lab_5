19f0fdbb1e4e5b48a6dc4496e326e979a4709dc7
Raven <redstoyningbros@gmail.com>
2020-12-12 17:19:27 +0200

    Implemented read\write from\to files functions

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
+from typing import Tuple, List
 from finite_state_automate import deterministic_state_automate as dfa
 
+INPUT_FILENAME = "search.in"
+OUTPUT_FILENAME = "search.out"
+
+
+def get_input_strings_for_file(filename: str) -> Tuple[str, str]:
+    with open(filename, "r") as file:
+        input_string = file.readline().strip()
+        pattern_string = file.readline().strip()
+    return input_string, pattern_string
+
+
+def write_result_list_to_file(filename: str, results: List[Tuple[int, int]]):
+    with open(filename, "w") as file:
+        for result in results:
+            file.write(f"{result[0]} {result[1]}\n")
+
+
 if __name__ == '__main__':
-    input_string = "HelloWorldIAmWritingRandomTextWithRepeatingLetters"
-    search_string = "ing"
-    dfa.search_substring(input_string, search_string, print)
+    string, pattern = get_input_strings_for_file(INPUT_FILENAME)
+    results_list = []
+    dfa.search_substring(string, pattern, lambda start, end: results_list.append((start, end)))
+    write_result_list_to_file(OUTPUT_FILENAME, results_list)
