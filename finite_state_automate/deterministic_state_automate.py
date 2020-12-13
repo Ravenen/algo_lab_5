703441d4d51b199c1a123d6ecabdaf62b0620ec4
Raven <redstoyningbros@gmail.com>
2020-12-14 01:54:15 +0200

    Updated algorithm to use Knuth-Morris-Pratt

--- finite_state_automate/deterministic_state_automate.py
+++ finite_state_automate/deterministic_state_automate.py
@@ -1,33 +1,33 @@
-from typing import Callable, List
+from typing import Callable, List, Dict
 
 
-def get_longest_prefix_suffix(pattern_string: str, code: int, current_state: int):
-    temporary_string = pattern_string[:current_state] + chr(code) + pattern_string[current_state + 1:]
-    result = 0
-    for possible_state in range(current_state):
-        for offset in range(possible_state + 1):
-            if ord(pattern_string[offset]) != ord(temporary_string[current_state - possible_state + offset]):
-                break
-        else:
-            result = max(result, possible_state + 1)
-    return result
-
-
-def get_next_state_by_code(pattern_string: str, code: int, current_state: int):
-    if ord(pattern_string[current_state]) == code:
+def get_next_state_by_letter(pattern_string: str, letter: str, current_state: int, longest_prefix_suffix: int,
+                             transition_function):
+    if current_state < len(pattern_string) and pattern_string[current_state] == letter:
         return current_state + 1
-    return get_longest_prefix_suffix(pattern_string, code, current_state)
+    return transition_function[longest_prefix_suffix][letter]
+
+
+def fill_in_zero_state(transition_function: List[Dict[str, int]], pattern_string: str, alphabet: List[str]):
+    for letter in alphabet:
+        transition_function[0][letter] = 0
+    transition_function[0][pattern_string[0]] = 1
 
 
 def generate_transition_function(pattern_string: str, alphabet: List[str]):
-    alphabet_length = len(alphabet)
     number_of_states = len(pattern_string) + 1
 
-    transition_function = [[0 for i in range(alphabet_length)] for j in range(number_of_states)]
+    transition_function = [{letter: 0 for letter in alphabet} for j in range(number_of_states)]
 
-    for state in range(number_of_states - 1):
-        for letter_index, letter in enumerate(alphabet):
-            transition_function[state][letter_index] = get_next_state_by_code(pattern_string, ord(letter), state)
+    fill_in_zero_state(transition_function, pattern_string, alphabet)
+
+    longest_prefix_suffix = 0
+    for state in range(1, number_of_states):
+        for letter in alphabet:
+            transition_function[state][letter] = get_next_state_by_letter(pattern_string, letter, state,
+                                                                          longest_prefix_suffix, transition_function)
+        if state < number_of_states - 1:
+            longest_prefix_suffix = transition_function[longest_prefix_suffix].get(pattern_string[state])
     return transition_function
 
 
@@ -38,9 +38,8 @@ def search_substring(input_string: str, pattern_string: str, output_function: Ca
 
     state = 0
     for index, letter in enumerate(input_string):
-        state = transition_function[state][alphabet.index(letter)]
+        state = transition_function[state][letter]
         if state == len(pattern_string):
             start_index = index - len(pattern_string) + 1
             end_index = index
             output_function(start_index, end_index)
-            state = 0
