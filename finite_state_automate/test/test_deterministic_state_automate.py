c4d251786856fdd9254704ea70fea32b11cc058c
Raven <redstoyningbros@gmail.com>
2020-12-14 02:38:19 +0200

    Updated tests

--- finite_state_automate/test/test_deterministic_state_automate.py
+++ finite_state_automate/test/test_deterministic_state_automate.py
@@ -4,28 +4,18 @@ from ..deterministic_state_automate import *
 
 
 class Test(unittest.TestCase):
-    def test_get_longest_prefix_suffix(self):
-        self.assertEqual(0, get_longest_prefix_suffix("ABCD", ord('B'), 3))
-        self.assertEqual(2, get_longest_prefix_suffix("ABAD", ord('B'), 3))
-        self.assertEqual(1, get_longest_prefix_suffix("ABAD", ord('A'), 3))
-        self.assertEqual(0, get_longest_prefix_suffix("ACACAGA", ord('C'), 6))
-        self.assertEqual(4, get_longest_prefix_suffix("ACACAGA", ord('C'), 5))
-        self.assertEqual(3, get_longest_prefix_suffix("ABCABDFF", ord('C'), 5))
-
-    def test_get_next_state_by_code(self):
-        self.assertEqual(0, get_next_state_by_code("ABCD", ord('B'), 2))
-        self.assertEqual(2, get_next_state_by_code("ABCD", ord('B'), 1))
-        self.assertEqual(1, get_next_state_by_code("ABCD", ord('A'), 2))
-        self.assertEqual(1, get_next_state_by_code("ABCABDFF", ord('A'), 2))
-        self.assertEqual(4, get_next_state_by_code("ABCABDFF", ord('A'), 3))
 
     def test_generate_transition_function(self):
         self.assertListEqual(
-            [[1, 0, 0, 0], [1, 2, 0, 0], [3, 0, 0, 0], [1, 4, 0, 0], [5, 0, 0, 0], [1, 4, 6, 0], [7, 0, 0, 0],
-             [0, 0, 0, 0]], generate_transition_function("ACACAGA", ['A', 'C', 'G', 'T']))
-        self.assertListEqual([[1], [2], [3], [0]], generate_transition_function("AAA", ['A']))
-        self.assertListEqual([[1, 0, 0, 0], [1, 2, 0, 0], [1, 0, 3, 0], [1, 0, 0, 4], [0, 0, 0, 0]],
-                             generate_transition_function("ABCD", ['A', 'B', 'C', 'D']))
+            [{'A': 1, 'C': 0, 'G': 0, 'T': 0}, {'A': 1, 'C': 2, 'G': 0, 'T': 0}, {'A': 3, 'C': 0, 'G': 0, 'T': 0},
+             {'A': 1, 'C': 4, 'G': 0, 'T': 0}, {'A': 5, 'C': 0, 'G': 0, 'T': 0}, {'A': 1, 'C': 4, 'G': 6, 'T': 0},
+             {'A': 7, 'C': 0, 'G': 0, 'T': 0}, {'A': 1, 'C': 2, 'G': 0, 'T': 0}],
+            generate_transition_function("ACACAGA", ['A', 'C', 'G', 'T']))
+        self.assertListEqual([{'A': 1}, {'A': 2}, {'A': 3}, {'A': 3}], generate_transition_function("AAA", ['A']))
+        self.assertListEqual(
+            [{'A': 1, 'B': 0, 'C': 0, 'D': 0}, {'A': 1, 'B': 2, 'C': 0, 'D': 0}, {'A': 1, 'B': 0, 'C': 3, 'D': 0},
+             {'A': 1, 'B': 0, 'C': 0, 'D': 4}, {'A': 1, 'B': 0, 'C': 0, 'D': 0}],
+            generate_transition_function("ABCD", ['A', 'B', 'C', 'D']))
 
     def test_search_substring(self):
         def append_to_list(input_list):
@@ -37,9 +27,18 @@ class Test(unittest.TestCase):
         output_list_first = []
         search_substring("ABCABCABCABDABC", "ABCABDA", append_to_list(output_list_first))
         self.assertListEqual([(6, 12)], output_list_first)
+
         output_list_second = []
         search_substring("AABAACAAD", "AA", append_to_list(output_list_second))
         self.assertListEqual([(0, 1), (3, 4), (6, 7)], output_list_second)
+
+        output_list_third = []
+        search_substring("AAAAAAAC", "AA", append_to_list(output_list_third))
+        self.assertListEqual([(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6)], output_list_third)
+
+        output_list_forth = []
+        search_substring("HelloWorld", "test", append_to_list(output_list_forth))
+        self.assertListEqual([], output_list_forth)
 
 
 if __name__ == '__main__':
